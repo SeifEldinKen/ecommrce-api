3fd43d467cd8dd15a377e74c3c2aadc42193d68f
Seif Eldin <seifeldinrefaat@gmail.com>
2021-09-29 13:51:13 +0200

    Handle errors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const morgan = require('morgan')
-const createError = require('http-error')
+const createError = require('http-errors')
 require('dotenv').config()
 
 // Create App 
 const app = express()
 
+
+app.use(async (req, res, next) => {
+    next(createError.NotFound("Not found"))
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.send({
+        error: {
+            status: error.status || 500,
+            message: error.message 
+        }
+    })
+})
 
 
 // Create Server
